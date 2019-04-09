68f464355308216907b3b0b17926926590e62a8e
ritchie46 <ritchie46@gmail.com>
2019-04-09 08:52:13 +0200

    dxf to data dir

--- anastruct/dxfloader/test.py
+++ anastruct/dxfloader/test.py
@@ -1,15 +1,9 @@
 import os
-
 from anastruct.fem.system import SystemElements
 
-THIS_SCRIPT_DIR_PATH = (os.path.dirname(os.path.abspath(__file__)))
-DXF_FILE_NAME = 'test.dxf'
-DXF_FILE_PATH = os.path.join(THIS_SCRIPT_DIR_PATH, DXF_FILE_NAME)
+DXF_FILE_PATH = os.path.join(os.path.dirname(__file__), 'data/test.dxf')
 
 ss = SystemElements()
 
 ss.load_dxf_data_to_system(DXF_FILE_PATH)
-
 ss.show_structure(annotations=True)
-ss.solve()
-ss.show_displacement()
